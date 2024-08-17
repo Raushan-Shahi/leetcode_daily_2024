76cad15c120e9765563808517d2f983c3ac98db1
Raushan-Shahi <raushanshahi2001@gmail.com>
2024-08-17 23:54:43 +0530

    aug 17

--- August/aug_17.cpp
+++ August/aug_17.cpp
@@ -1,128 +1,30 @@
-# Recursion solution gives tle at 47 test case 
-
 class Solution {
 public:
-    typedef long long ll;
-    ll find_sum(vector<vector<int>>&points,int row,int col){
-
-        //base case
-        if(row>=points.size()){
-            return 0;
-        }
-
-        ll ans=0;
-
-        for(int i=0;i<points[0].size();i++){
-           ll sum=points[row][i]-abs(i-col)+find_sum(points,row+1,i);
-           ans=max(ans,sum);
-        }
-
-        return ans;
-
-    }
-    long long maxPoints(vector<vector<int>>& points) {
+    long maxPoints(vector<vector<int>>& points){
+        int m = points.size();
+        int n = points[0].size();
+        vector<long> dp(n, 0);
+        long ans = 0;
         
-        int n=points.size();
-        int m=points[0].size();
+        for (int i = 0; i < m; ++i) {
 
-        ll ans=INT_MIN;
-
-        for(int i=0;i<m;i++){
-           ll sum=points[0][i]+find_sum(points,1,i);
-            ans=max(ans,sum);
-        }
-
-        return ans;
-    }
-};
-
-# Memoization gives tle at 143 test case
-
-class Solution {
-public:
-    typedef long long ll;
-    ll find_sum(vector<vector<int>>&points,int row,int col,vector<vector<int>>&dp){
-
-        //base case
-        if(row>=points.size()){
-            return 0;
-        }
-        
-        if(dp[row][col]!=-1){
-            return dp[row][col];
-        }
-        ll ans=0;
-
-        for(int i=0;i<points[0].size();i++){
-           ll sum=points[row][i]-abs(i-col)+find_sum(points,row+1,i,dp);
-           ans=max(ans,sum);
-        }
-
-        return dp[row][col]=ans;
-
-    }
-    long long maxPoints(vector<vector<int>>& points) {
-        
-        int n=points.size();
-        int m=points[0].size();
-
-        ll ans=INT_MIN;
-
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-
-        for(int i=0;i<m;i++){
-           ll sum=points[0][i]+find_sum(points,1,i,dp);
-            ans=max(ans,sum);
-        }
-
-        return ans;
-    }
-};
-
-# Tabulation accepted 
-
-class Solution {
-public:
-    long long maxPoints(vector<vector<int>>& points) {
-        
-        int n=points.size();
-        int m=points[0].size();
-
-        vector<vector<long long>>dp(n+1,vector<long long>(m+1,-1));
-
-        for(int i=0;i<m;i++){
-            dp[0][i]=points[0][i];
-        }
-
-        for(int i=1;i<n;i++){
-
-            vector<long long>left(m),right(m);
-            
-            left[0]=dp[i-1][0];
-
-            for(int j=1;j<m;j++){
-             left[j]=max(left[j-1]-1,dp[i-1][j]);
-            }
-
-            right[m-1]=dp[i-1][m-1];
-
-            for(int j=m-2;j>=0;j--){
-             right[j]=max(right[j+1]-1,dp[i-1][j]);
-            }
-
-            for(int j=0;j<m;j++){
-                dp[i][j]=points[i][j]+max(left[j],right[j]);
+            for (int j = 0; j < n; ++j) {
+                dp[j] += points[i][j];
             }
             
+            for (int j = 1; j < n; ++j) {
+                dp[j] = max(dp[j], dp[j - 1] - 1);
+            }
+            
+            for (int j = n - 2; j >= 0; --j) {
+                dp[j] = max(dp[j], dp[j + 1] - 1);
+            }
         }
-
-        long long ans=INT_MIN;
-
-        for(int i=0;i<m;i++){
-            ans=max(ans,dp[n-1][i]);
+        
+        for (int i = 0; i < n; ++i) {
+            ans = max(ans, dp[i]);
         }
-
+        
         return ans;
     }
 };
- 
